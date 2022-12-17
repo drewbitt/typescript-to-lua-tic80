b20003046714399d065f7f0976362c73caceb421
drewbitt <andrew@drewbitt.com>
2022-12-17 01:55:54 -0500

    fix: Support modularization

--- index.ts
+++ index.ts
@@ -1,30 +1,32 @@
- /**
-  title:  game title
-  author: drewbitt
-  desc:   short description
-  script: lua
-  */
-function addTitle () { /* Empty for hack to add title to lua */ }
+// Do not ever use a module for TIC() as TIC-80 will not find `local function TIC()`
+const global = globalThis as typeof globalThis & { TIC: () => void };
+if (global.TIC === undefined) {
+	global.TIC = () => TIC();
+}
 
-let t = 0
-let x = 96
-let y = 24
+let t = 0;
+let x = 96;
+let y = 24;
 
 function TIC() {
-  if (btn(0)) y--
-  if (btn(1)) y++
-  if (btn(2)) x--
-  if (btn(3)) x++
-  cls(13)
-  spr(1 + (floorDivision((t % 60), 30)) * 2, x, y, 14, 3, 0, 0, 2, 2)
-  print("HELLO WORLD!", 84, 84)
-  t++
+	if (btn(0)) y--;
+	if (btn(1)) y++;
+	if (btn(2)) x--;
+	if (btn(3)) x++;
+	cls(13);
+	spr(1 + floorDivision(t % 60, 30) * 2, x, y, 14, 3, 0, 0, 2, 2);
+	print("HELLO WORLD!", 84, 84);
+	t++;
 }
 
 declare const floorDivision: LuaFloorDivision<number, number, number>;
 
 /**
- *  <TILES>
+ *  title:  game title
+ author: drewbitt
+ desc:   short description
+
+ <TILES>
  001:eccccccccc888888caaaaaaaca888888cacccccccacc0ccccacc0ccccacc0ccc
  002:ccccceee8888cceeaaaa0cee888a0ceeccca0ccc0cca0c0c0cca0c0c0cca0c0c
  003:eccccccccc888888caaaaaaaca888888cacccccccacccccccacc0ccccacc0ccc
@@ -53,4 +55,4 @@ declare const floorDivision: LuaFloorDivision<number, number, number>;
  000:100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
  </TRACKS>
  */
-function addExtras () { /* Empty for hack to add tiles,sfx etc to lua */ }
+function addAnnotations() {/* Empty method for hack to add TIC-80 annotations to lua */}
